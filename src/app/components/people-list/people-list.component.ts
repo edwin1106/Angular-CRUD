50c803b8c2a86f8f7a6b0f97a7c02d99f24c8a57
edwinosoriogaviria <edwinosoriogaviria@hotmail.com>
2019-10-11 21:14:43 -0500

    Conect frontend with backend

--- src/app/components/people-list/people-list.component.ts
+++ src/app/components/people-list/people-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+//import { PersonaService } from 'src/app/service/persona.service';
+import {Persona} from '../../model/persona'
+import {PersonaService} from '../../service/persona.service'
 
 @Component({
   selector: 'app-people-list',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeopleListComponent implements OnInit {
 
-  constructor() { }
+  personas: Persona[] = [];
+  constructor(private personaService: PersonaService) { }
 
   ngOnInit() {
+
+  this.personaService.listarPersona().subscribe(
+    (personas) => {
+      this.personas = personas;
+    }
+
+  );
   }
 
 }
--- src/app/util/endpoints.ts
+++ src/app/util/endpoints.ts
@@ -1,6 +1,4 @@
 export class Endpoints {
-  public static readonly LISTAR="http://localhost:8181/personas/personas";
+  public static readonly LISTAR="http://localhost:8080/personas/personas";
   public static readonly INSERTAR="http://localhost:8181/personas/persona";
-
-
 }
